71b30a292da3f81140e635c7b343b93f41b3e64a
Harold Jean <haroldjean38@yahoo.com>
2023-01-24 14:10:35 -0500

    Added container to each js and created css

--- src/components/Green.js
+++ src/components/Green.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Green.css';
 
 export default function Green() {
   return (
-    <div className="container">
+    <div className="green-container">
       <h1>Green</h1>
+      <Link to="/">Back to Main</Link>
     </div>
   );
 }
--- src/components/Red.js
+++ src/components/Red.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Red.css';
 
 export default function Red() {
   return (
-    <div className="container">
+    <div className="red-container">
       <h1>Red</h1>
+      <Link to="/">Back to Main</Link>
     </div>
   );
 }
