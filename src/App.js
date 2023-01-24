25f1a8f1e471e04f6ee3ba5701e06ea482686b01
Harold Jean <haroldjean38@yahoo.com>
2023-01-24 08:45:35 -0500

    installed NotFound Route into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import NotFound from './components/NotFound.js';
 
 function App() {
   return (
  <>
   <Switch>
     <Route></Route>
+    <Route path="*" component={NotFound}/>
   </Switch>
  </>
   );
