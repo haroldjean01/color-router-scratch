3e53b95e546208af99087837d2b52e940f8306fa
Harold Jean <haroldjean38@yahoo.com>
2023-01-24 11:51:05 -0500

    fixed Render on page. by adding to RGB js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
 import { Route, Switch, NavLink } from 'react-router-dom';
 import NotFound from './components/NotFound.js';
-import RGB from './components/RGB/RGB.js';
+import Green from './components/Green.js';
+import Red from './components/Red.js';
+import Blue from './components/Blue.js';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Switch>
-        <Route path="/" component={RGB} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/">RGB</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Red">Red</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Green">Green</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Blue">Blue</NavLink>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/Red">
+            <Red />
+          </Route>
+          <Route path="/Green">
+            <Green />
+          </Route>
+          <Route path="/Blue">
+            <Blue />
+          </Route>
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/RGB/RGB.js
+++ src/components/RGB/RGB.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { NavLink, Router } from 'react-router-dom';
 
 export default function RGB() {
   return (
-    <nav className="nav-wrapper yellow">
-      <div className="container">
-        <a className="rgb">RGB</a>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/red">Red</a>
-          </li>
-          <li>
-            <a href="/blue">Blue</a>
-          </li>
-          <li>
-            <a href="/green">Green</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Router>
+      <nav className="nav-wrapper yellow">
+        <div className="container">
+          <a className="rgb">RGB</a>
+          <ul>
+            <li>
+              <NavLink href="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink href="/red">Red</NavLink>
+            </li>
+            <li>
+              <NavLink href="/blue">Blue</NavLink>
+            </li>
+            <li>
+              <NavLink href="/green">Green</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Router>
   );
 }
