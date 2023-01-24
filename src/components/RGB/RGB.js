9bee3be01e785918b3f1bb7ba2015feaf89d5372
Harold Jean <haroldjean38@yahoo.com>
2023-01-24 14:38:06 -0500

    added Navlink colors to RGBjs

--- src/components/RGB/RGB.js
+++ src/components/RGB/RGB.js
@@ -6,5 +6,12 @@ export default function RGB() {
   const { r, g, b } = useParams();
   const color = `rgb(${r}, ${g}, ${b})`;
 
-  return <div style={{ backgroundColor: color }}></div>;
+  return (
+    <div className="rgb-container" style={{ backgroundColor: `rbg(${r}, ${g}, ${b})` }}>
+      <div className="color-container">
+        <NavLink to={'/rgb/100/100/255'}></NavLink>
+      </div>
+      <h2>{color}</h2>
+    </div>
+  );
 }
