508e5f90b9e869caddfc73cc143f4554cf3a9e21
Harold Jean <haroldjean38@yahoo.com>
2023-01-24 20:39:13 -0500

    refactored code to one Route in add js and changed height to 100%

--- src/App.js
+++ src/App.js
@@ -5,34 +5,29 @@ import Green from './components/Green.js';
 import Red from './components/Red.js';
 import Blue from './components/Blue.js';
 import { BrowserRouter as Router } from 'react-router-dom';
+import RGB from './components/RGB/RGB.js';
 
 function App() {
   return (
     <Router>
       <div className="app-container">
         <div className="buttons-container">
-          <NavLink to="/255/100/100">
+          <NavLink to="/rgb/255/100/100">
             <button>Red</button>
           </NavLink>
-          <NavLink to="/100/255/100">
+          <NavLink to="/rgb/100/255/100">
             <button>Green</button>
           </NavLink>
-          <NavLink to="/100/100/255">
+          <NavLink to="/rgb/100/100/255">
             <button>Blue</button>
           </NavLink>
         </div>
         <Switch>
           <Route exact path="/">
-            <Redirect to="/255/100/100" />
+            <Redirect to="/rgb/255/100/100" />
           </Route>
-          <Route exact path="/255/100/100">
-            <Red />
-          </Route>
-          <Route path="/100/255/100">
-            <Green />
-          </Route>
-          <Route path="/100/100/255">
-            <Blue />
+          <Route exact path="/rgb/:r/:g/:b">
+            <RGB />
           </Route>
           <Route exact path="*" component={NotFound} />
         </Switch>
